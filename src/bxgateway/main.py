ca3025c693eb668c05a3ee066ffc00aaeecc4196
y3sh <joshhibschman@gmail.com>
2018-06-26 13:21:46 -0500

    BX-34 de-duping main.py

--- src/bxgateway/main.py
+++ src/bxgateway/main.py
@@ -5,98 +5,31 @@
 # Startup script for nodes
 #
 
-import ConfigParser
 import argparse
 
+from bxcommon.util import startup_util
 from connections import *
-from bxcommon.utils import *
 
 # All parameters that are parsed from the config file.
-ALL_PARAMS = ['my_ip', 'my_port', 'peers', 'my_idx', 'manager_idx', 'node_params', 'node_addr', 'log_path',
-              'log_stdout']
+ALL_PARAMS = [
+    'my_ip',
+    'my_port',
+    'peers',
+    'my_idx',
+    'manager_idx',
+    'log_path',
+    'log_stdout',
+    'node_params',
+    'node_addr'
+]
 MAX_NUM_CONN = 8192
-
-# Some websites are blocked in certain jurisdictions, so we try multiple websites to see whichever one works.
-WEBSITES_TO_TRY = ['www.google.com', 'www.alibaba.com']
-
-
-# Returns the local internal IP address of the node.
-# If the node is behind a NAT or proxy, then this is not the externally visible IP address.
-def getmyip():
-    for website in WEBSITES_TO_TRY:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(5)
-            s.connect((website, 80))
-            return s.getsockname()[0]
-        except socket.timeout:
-            continue
-
-    raise Exception("Could not find any local name!")
-
-
-# Parse the config filename and return a params dictionary with the params from ALL_PARAMS
-def parse_config_file(filename, localname):
-    client_config = ConfigParser.ConfigParser()
-    client_config.read(filename)
-
-    config_params = {}
-    for param_name in ALL_PARAMS:
-        config_params[param_name] = getparam(client_config, localname, param_name)
-
-    return client_config, config_params
-
-
-# Gets the param "pname" from the config file.
-# If the param exists under the localname, we use that one. Otherwise, we use
-# the param under default.
-def getparam(client_config, local_name, param_name):
-    if not client_config:
-        return None
-
-    try:
-        return client_config.get(local_name, param_name)
-    except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
-        try:
-            return client_config.get("default", param_name)
-        except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
-            return None
-
-
-# Parse the peers file and returns a dictionary of {cls : list of ip, port pairs}
-# to tell the node which connection types to instantiate.
-# Parse the peers string and return two lists:
-#   1) A list of relays that are internal nodes in our network
-#   2) A list of trusted peers that we will connect to.
-def parse_peers(peers_string):
-    nodes = {}
-
-    if peers_string is not None:
-        for line in peers_string.split(","):
-            peers = line.strip().split()
-
-            peer_ip = None
-            while peer_ip is None:
-                try:
-                    peer_ip = socket.gethostbyname(peers[0])
-                except socket.error:
-                    print "Caught socket error while resolving name! Retrying..."
-                    time.sleep(0.1)
-                    peer_ip = None
-
-            peer_port = int(peers[1])
-            peer_idx = int(peers[2])
-            nodes[peer_idx] = (peer_ip, peer_port)
-
-    return nodes
-
 
 if __name__ == '__main__':
     # Log our pid to a file.
     with open("relay.pid", "w") as f:
         f.write(str(os.getpid()))
 
-    # Local name:
+    # Returns a dictionary of args like "-n" to arrays of values
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config-name",
                         help="Name of section to read from config.cfg. By default will read a section using this node's"
@@ -117,26 +50,28 @@ if __name__ == '__main__':
 
     # The local name is the section of the config.cfg we will read
     # It can be specified with -c or will be the local ip of the machine
-    mylocalname = opts.config_name or getmyip()
+    my_local_name = opts.config_name or startup_util.get_my_ip()
 
     # Parse the config file.
     configFileName = "config.cfg"
-    config, params = parse_config_file(configFileName, mylocalname)
+    config, params = startup_util.parse_config_file(configFileName, my_local_name, ALL_PARAMS)
 
     # Set basic variables.
     # XXX: Add assert statements to make sure these make sense.
     ip = opts.network_ip or params['my_ip']
-    assert ip is not None, "Your IP address is None!"
+    assert ip is not None, "Your IP address is not specified in config.cfg or as --network-ip. Check that the '-n' " \
+                           "argument reflects the name of a section in config.cfg!"
+
     port = int(opts.port or params['my_port'])
 
     log_setmyname("%s:%d" % (ip, port))
     log_path = opts.log_path or params['log_path']
     use_stdout = opts.to_stdout or params['log_stdout']
     log_init(log_path, use_stdout)
-    log_debug("My own IP for config purposes is {0}".format(mylocalname))
+    log_debug("My own IP for config purposes is {0}".format(my_local_name))
 
     # Initialize the node and register the peerfile update signal to USR2 signal.
-    relay_nodes = parse_peers(opts.peers or params['peers'])
+    relay_nodes = startup_util.parse_peers(opts.peers or params['peers'])
 
     node_param_list = {}
     if params['node_params']:
@@ -146,7 +81,7 @@ if __name__ == '__main__':
 
     if node_param_list:
         for param in node_param_list:
-            node_params[param] = getparam(config, mylocalname, param)
+            node_params[param] = startup_util.getparam(config, my_local_name, param)
 
     if opts.blockchain_node:
         params['node_addr'] = opts.blockchain_node
