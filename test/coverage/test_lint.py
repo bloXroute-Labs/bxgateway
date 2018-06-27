b61ac9b86a38dcfab85a5f74cbffdf5f4bcbde89
y3sh <joshhibschman@gmail.com>
2018-06-27 13:54:09 -0500

    circleci updates

--- test/coverage/test_lint.py
+++ test/coverage/test_lint.py
@@ -5,6 +5,6 @@ from bxcommon.test_util.pylint_reporter import *
 
 class LintTests(unittest.TestCase):
     def test_lint_score(self):
-        lint_score = lint_directory("../src/bxrelay")
+        lint_score = lint_directory("../src/bxgateway")
 
         self.assertGreater(lint_score, MIN_PYLINT_SCORE, "Lint score was too low, please lint and refactor your code.")
