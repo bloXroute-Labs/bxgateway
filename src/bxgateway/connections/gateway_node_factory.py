6f54d7100990dc8ee3e9e243e64c9ed5720cac77
Sergey Ilin <ilinserg@gmail.com>
2019-04-17 11:16:43 -0500

    Remove dependency on OpenSSL for Bitcoin gateways (#125)

--- src/bxgateway/connections/gateway_node_factory.py
+++ src/bxgateway/connections/gateway_node_factory.py
@@ -1,11 +1,11 @@
 from bxgateway.connections.btc.btc_gateway_node import BtcGatewayNode
-from bxgateway.connections.eth.eth_gateway_node import EthGatewayNode
 
 
 def get_gateway_node_type(blockchain_protocol, blockchain_network):
 
     # TODO: This is temporary logic that will be replaced with list of valid protocols and networks from SDN
     if blockchain_protocol == "Ethereum":
+        from bxgateway.connections.eth.eth_gateway_node import EthGatewayNode
         return EthGatewayNode
 
     return BtcGatewayNode
