f4ca870c6d009f58ee030256a9a982c8df3f3a36
Shane van Coller <vc.shane@gmail.com>
2019-04-23 20:06:48 -0500

    fix docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,8 +14,8 @@ STARTUP="$PYTHON main.py $@"
 echo "$PROGNAME: Starting $STARTUP"
 if [ "$(id -u)" = '0' ]; then
     # if running as root, chown and step-down from root
-    find . \! -type l -user $USER -exec chown $USER:$GROUP '{}' +
-    find ../bxcommon \! -type l -user $USER -exec chown $USER:$GROUP '{}' +
+    find . \! -type l \! -user $USER -exec chown $USER:$GROUP '{}' +
+    find ../bxcommon \! -type l \! -user $USER -exec chown $USER:$GROUP '{}' +
     cd $WORKDIR
     exec su-exec $USER $STARTUP
 else
