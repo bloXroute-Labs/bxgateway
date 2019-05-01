2f1655948dd430823797c1c7230a7ed81635cdd3
avrahamblx <46294481+avrahamblx@users.noreply.github.com>
2019-05-01 12:11:43 -0500

    added missing implementation follwoed by bx-733 - memory_refactoring (#135)

--- src/bxgateway/services/btc_transaction_service.py
+++ src/bxgateway/services/btc_transaction_service.py
@@ -1,4 +1,6 @@
 from bxcommon.services.transaction_service import TransactionService
+from bxcommon.utils import convert
+
 import task_pool_executor as tpe  # pyre-ignore for now, figure this out later (stub file or Python wrapper?)
 
 
@@ -15,16 +17,15 @@ class BtcTransactionService(TransactionService):
 
     def _remove_transaction_by_short_id(self, short_id):
         if short_id in self._short_id_to_tx_hash:
-            transaction_hash = self._short_id_to_tx_hash.pop(short_id)
-            if transaction_hash in self._tx_hash_to_short_ids:
-                short_ids = self._tx_hash_to_short_ids[transaction_hash]
-
+            transaction_cache_key = self._short_id_to_tx_hash.pop(short_id)
+            if transaction_cache_key in self._tx_hash_to_short_ids:
+                short_ids = self._tx_hash_to_short_ids[transaction_cache_key]
                 # Only clear mapping and _tx_hash_to_contents if last SID assignment
                 if len(short_ids) == 1:
-                    del self._tx_hash_to_short_ids[transaction_hash]
-                    cpp_hash = tpe.Sha256(tpe.InputBytes(transaction_hash.binary))
+                    del self._tx_hash_to_short_ids[transaction_cache_key]
+                    cpp_hash = tpe.Sha256(tpe.InputBytes(convert.hex_to_bytes(transaction_cache_key)))
                     del self.cpp_tx_hash_to_short_ids[cpp_hash]
-                    if transaction_hash in self._tx_hash_to_contents:
-                        del self._tx_hash_to_contents[transaction_hash]
+                    if transaction_cache_key in self._tx_hash_to_contents:
+                        del self._tx_hash_to_contents[transaction_cache_key]
                 else:
                     short_ids.remove(short_id)
