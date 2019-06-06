b0892e98b133749f3c07d9670b5b1a42554c6523
Kevin Chen <kevin@bloxroute.com>
2019-06-06 10:33:10 -0500

    Sets larger send buffer size for blockchain connection socket (#181)
    * Add custom send buffer size for gateway
    
    * fix for message tracking
    
    * Improved behavior and include backup mechanisms

--- src/bxgateway/connections/abstract_gateway_blockchain_connection.py
+++ src/bxgateway/connections/abstract_gateway_blockchain_connection.py
@@ -1,5 +1,8 @@
+import socket
+
 from bxcommon.connections.abstract_connection import AbstractConnection
 from bxcommon.connections.connection_type import ConnectionType
+from bxcommon.utils import logger
 from bxgateway import gateway_constants
 
 
@@ -8,6 +11,24 @@ class AbstractGatewayBlockchainConnection(AbstractConnection):
 
     def __init__(self, sock, address, node, from_me=False):
         super(AbstractGatewayBlockchainConnection, self).__init__(sock, address, node, from_me)
+
+        # Requires OS tuning (for Linux here; MacOS and windows require different commands):
+        # echo 'net.core.wmem_max=16777216' >> /etc/sysctl.conf && sysctl -p
+        # cat /proc/sys/net/core/wmem_max # to check
+        previous_buffer_size = sock.socket_instance.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
+        try:
+            sock.socket_instance.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, gateway_constants.BLOCKCHAIN_SOCKET_SEND_BUFFER_SIZE)
+            logger.info("Set socket send buffer size for blockchain connection to {}",
+                        sock.socket_instance.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF))
+        except Exception as e:
+            logger.error("Could not set socket send buffer size for blockchain connection: {}", e)
+
+        set_buffer_size = sock.socket_instance.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
+        if set_buffer_size != gateway_constants.BLOCKCHAIN_SOCKET_SEND_BUFFER_SIZE:
+            logger.warn("Socket buffer size set was unsuccessful, and was instead set to {}. Reverting.",
+                        set_buffer_size)
+            sock.socket_instance.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, previous_buffer_size)
+
         self.message_converter = None
         self.connection_protocol = None
         self.is_server = False
