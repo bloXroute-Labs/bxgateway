7ec53c14f6c4499886505297929932a673e217e2
sinai yoktan <44772610+sinai-yoktan@users.noreply.github.com>
2020-06-04 14:20:22 +0300

    bx1715/add network protocol to account (#647)
    added network information to account,

--- test/unit/test_main.py
+++ test/unit/test_main.py
@@ -3,11 +3,11 @@ from unittest import skip
 
 from bxcommon.test_utils.abstract_test_case import AbstractTestCase
 from bxcommon.models.outbound_peer_model import OutboundPeerModel
+from bxcommon.models.bdn_account_model_base import BdnAccountModelBase
 
 from bxgateway import main
 
 
-@skip("Test doesnt pass because of MANIFEST path issues")
 class MainTest(AbstractTestCase):
 
     def test_parse_peer_string(self):
@@ -21,17 +21,17 @@ class MainTest(AbstractTestCase):
 
     def test_get_opts_outbound_peers_provided(self):
         sys.argv = ["main.py",
+                    "--blockchain-ip", "172.17.0.1",
                     "--blockchain-port", "9333",
                     "--blockchain-protocol", "Bitcoin",
                     "--blockchain-network", "Mainnet",
                     "--peer-relays", "127.0.0.1:8000,127.0.0.1:8001",
                     "--peer-gateways", "127.0.1.1:8000,127.0.1.0:8001",
                     "--source-version", "1.2.3",
-                    "--external-ip", "127.0.0.1",
                     "--external-port", "7000"]
         args = main.get_opts()
         self.assertEqual(9333, args.blockchain_port)
-        self.assertEqual("Bitcoin", args.blockchain_protocol)
+        self.assertEqual("bitcoin", args.blockchain_protocol)
         self.assertEqual("Mainnet", args.blockchain_network)
         self.assertEqual(2, len(args.peer_gateways))
         self.assertEqual(2, len(args.peer_relays))
@@ -39,15 +39,35 @@ class MainTest(AbstractTestCase):
 
     def test_get_opts_outbound_peers_not_provided(self):
         sys.argv = ["main.py",
+                    "--blockchain-ip", "172.17.0.1",
                     "--blockchain-port", "9333",
                     "--blockchain-protocol", "Bitcoin",
                     "--blockchain-network", "Mainnet",
                     "--source-version", "1.2.3",
-                    "--external-ip", "127.0.0.1",
                     "--external-port", "7000"]
         args = main.get_opts()
         self.assertEqual(9333, args.blockchain_port)
-        self.assertEqual("Bitcoin", args.blockchain_protocol)
+        self.assertEqual("bitcoin", args.blockchain_protocol)
+        self.assertEqual("Mainnet", args.blockchain_network)
+        self.assertEqual(0, len(args.peer_gateways))
+        self.assertEqual(0, len(args.peer_relays))
+        self.assertEqual(0, len(args.outbound_peers))
+
+    def test_get_opts_blockchain_info_not_provided(self):
+        sys.argv = ["main.py",
+                    "--blockchain-ip", "172.17.0.1",
+                    "--blockchain-port", "9333",
+                    "--source-version", "1.2.3",
+                    "--external-port", "7000"]
+        args = main.get_opts()
+        args.account_model = BdnAccountModelBase(
+            "fake_id", "fake_account_name", "fake_certificate", blockchain_network="Mainnet",
+            blockchain_protocol="Bitcoin"
+        )
+        args.post_init_tasks()
+
+        self.assertEqual(9333, args.blockchain_port)
+        self.assertEqual("bitcoin", args.blockchain_protocol)
         self.assertEqual("Mainnet", args.blockchain_network)
         self.assertEqual(0, len(args.peer_gateways))
         self.assertEqual(0, len(args.peer_relays))
