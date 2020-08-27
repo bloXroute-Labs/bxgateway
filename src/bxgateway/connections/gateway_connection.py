1606f34d1e15bda0523638c126fd379f88d6987e
Danielle <44942738+daniellemachpud@users.noreply.github.com>
2020-08-27 09:32:40 +0300

    call threaded request when submit events to the sdn (#825)

--- src/bxgateway/connections/gateway_connection.py
+++ src/bxgateway/connections/gateway_connection.py
@@ -105,7 +105,11 @@ class GatewayConnection(InternalNodeConnection["AbstractGatewayNode"]):
 
         if not self.from_me:
             self.log_trace("Connection established with peer: {}.", peer_id)
-            sdn_http_service.submit_gateway_inbound_connection(self.node.opts.node_id, self.peer_id)
+            self.node.requester.send_threaded_request(
+                sdn_http_service.submit_gateway_inbound_connection,
+                self.node.opts.node_id,
+                self.peer_id
+            )
 
         if self.node.connection_exists(ip, port):
             connection = self.node.connection_pool.get_by_ipport(ip, port)
@@ -226,4 +230,8 @@ class GatewayConnection(InternalNodeConnection["AbstractGatewayNode"]):
 
         if not self.from_me:
             if self.peer_id:
-                sdn_http_service.delete_gateway_inbound_connection(self.node.opts.node_id, self.peer_id)
+                self.node.requester.send_threaded_request(
+                    sdn_http_service.delete_gateway_inbound_connection,
+                    self.node.opts.node_id,
+                    self.peer_id
+                )
