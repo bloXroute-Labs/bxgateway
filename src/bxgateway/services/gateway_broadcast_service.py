e91a6fbdf58f7a3a5dd52934986c238e44c712cc
Kevin Chen <kevin@bloxroute.com>
2020-11-12 14:24:58 -0600

    BX-2223 minor refactor (#934)

--- src/bxgateway/services/gateway_broadcast_service.py
+++ src/bxgateway/services/gateway_broadcast_service.py
@@ -1,9 +1,16 @@
 from bxcommon.connections.abstract_connection import AbstractConnection
 from bxcommon.messages.abstract_message import AbstractMessage
-from bxcommon.services.broadcast_service import BroadcastService
+from bxcommon.services.broadcast_service import BroadcastService, BroadcastOptions
 
 
-class GatewayBroadcastService(BroadcastService[AbstractMessage, AbstractConnection]):
-    def should_broadcast_to_connection(self, message: AbstractMessage, connection: AbstractConnection) -> bool:
+class GatewayBroadcastService(
+    BroadcastService[AbstractMessage, AbstractConnection, BroadcastOptions]
+):
+    def should_broadcast_to_connection(
+        self,
+        message: AbstractMessage,
+        connection: AbstractConnection,
+        options: BroadcastOptions
+    ) -> bool:
         # gateway does not really care about network numbers
         return True
